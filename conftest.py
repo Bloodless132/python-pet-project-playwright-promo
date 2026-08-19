fbeb3e8e554aeaeffe5359594d4934538f9472b4
Mykyta Dziuba <mykyta.dziuba@intellias.com>
2026-08-19 08:24:19 +0300

    Setup project structure and add first UI automation test

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from playwright.sync_api import Page, Playwright
+
+
+@pytest.fixture
+def page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch()
+    page = browser.new_page()
+
+    yield page
+
+    browser.close()
